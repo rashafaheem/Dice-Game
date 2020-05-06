e9a8eafe245d52cb5bb64fcdf68aa8ed3888476f
Rasha Faheem <rashafaheem@github.com>
2020-05-06 13:20:54 +0530

    Updated

--- a.js
+++ a.js
@@ -1,6 +1,11 @@
 
 
-function myFunction() {
-  document.getElementById("click").innerHTML=(Math.floor(Math.random() * 6)+1);
-  }
+{
+    
+var myPix = new Array("d1.png","d2.png","d3.png","d4.png","d5.png","d6.png");
 
+function display_random_image()  {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("myPicture").src = myPix[randomNum];
+}
+}
