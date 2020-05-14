1159fa0fe65ea3681319373f3a88cd230b1d82c6
Rasha Faheem <rashafaheem@github.com>
2020-05-14 12:08:50 +0530

    Updated

--- a.js
+++ a.js
@@ -6,38 +6,37 @@ var p2Score = document.getElementById("player2Score");
 var score1 = 0;
 var score2 = 0;
 var moves = 0;
-var activePlayer;
-var diceTotal = 0;
+var randomNum;
 
-function score()
-{
-switch (scored) {
-  case "d1.png":
-    score = 1;
-    break;
-  case "d2.png":
-    score = 2;
-    break;
-  case "d3.png":
-    score = 3;
-    break;
-  case "d4.png":
-    score = 4;
-    break;
-  case "d5.png":
-    score = 5;
-    break;
-  case "d6.png":
-    score = 6;
-}
-}
+
 
 function rollDice()  
 {
-     var randomNum = Math.floor(Math.random() * myPix.length);
+     randomNum = Math.floor(Math.random() * myPix.length);
      document.getElementById("myPicture").src = myPix[randomNum];
      moves++;
      console.log(moves);
+     scored = myPix[randomNum];
+     switch (scored) {
+      case "d1.png":
+        score = 1;
+        break;
+      case "d2.png":
+        score = 2;
+        break;
+      case "d3.png":
+        score = 3;
+        break;
+      case "d4.png":
+        score = 4;
+        break;
+      case "d5.png":
+        score = 5;
+        break;
+      case "d6.png":
+        score = 6;
+    }
+   
 }
 
 
@@ -67,19 +66,20 @@ function switchChance()
 if (moves % 2 == 0) 
 {
   console.log("p2");
-  document.getElementById('player2').src='active.png';
+  document.getElementById('player2').src='active1.png';
   document.getElementById('player1').src='player1.png';
-  score2 = score2 + scored;
+  score2 = score2 + score;
   p2Score.innerHTML = score2;
+
 
 } 
 else {
   console.log("p1");
-  document.getElementById('player1').src='active.png';
+  document.getElementById('player1').src='active1.png';
   document.getElementById('player2').src='player2.png';
-  document.getElementById("player1Score").innerHTML = text;
-  score1 = score1 + scored;
+  score1 = score1 + score;
   p1Score.innerHTML = score1;
+  
 } 
 
 }
